67da4de899aab0dcfbb99013881477e16e4b566f
SlaVcE <102253539+SlaVcE14@users.noreply.github.com>
2022-12-21 21:41:48 +0100

    lab 8

--- labs/lab7_2.c
+++ labs/lab7_2.c
@@ -4,57 +4,35 @@
 
 
 int main(){
-
     int m, n;
-
     int a[MAX][MAX];
-
-
     scanf("%d%d",&m,&n);
 
     for(int i = 0; i < m; i++){
-
         for(int j = 0; j < n; j++){
-
             int num;
-
             scanf("%d",&num);
-
             a[i][j] = num;
         }
     }
-
     float zz[MAX][MAX];
-    for(int i = 0; i < n; i++){
 
+    for(int i = 0; i < n; i++){
         int min = a[0][i];
         int max = a[0][i];
-
-
         for(int j = 1; j < m; j++){
-
-
             if(a[j][i] < min){
                 min = a[j][i];
             }
             if(a[j][i] > max){
                 max = a[j][i];
             }
-
-
         }
-
-
-
         for(int j = 0; j < m; j++){
             int num = a[j][i];
-
             float num1 = (float)(num - min)/(float)(max - min);
-
             zz[j][i] = num1;
-
         }
-
     }
 
     for (int i = 0; i < m; ++i) {
@@ -63,5 +41,4 @@ int main(){
         }
         printf("\n");
     }
-
 }
